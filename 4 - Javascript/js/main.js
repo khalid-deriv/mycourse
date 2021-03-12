01d281cbc6fb4ee5093e85a0e4135328e44fe141
yashim-deriv <yashim@deriv.com>
2021-03-12 12:04:24 +0800

    Added game functionality

--- 4 - Javascript/js/main.js
+++ 4 - Javascript/js/main.js
@@ -1,5 +1,9 @@
 import initializeControls from "./controls.js";
+import initializeGame from "./game.js";
 
 window.onload = function () {
   initializeControls();
+
+  const start_game_button = document.getElementById("start_game");
+  start_game_button.addEventListener("onClick", initializeGame);
 };
